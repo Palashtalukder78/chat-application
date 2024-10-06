e60f63efd1243677e04ca6da7e660af3932a23e6
Md Ariful Islam Palash <Md Ariful Islam Palash>
2024-10-06 14:52:56 +0600

    authenticated request with prepare header in apiSlice

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Conversation from "./pages/Conversation";
 import Inbox from "./pages/Inbox";
+import useAuthChecked from "./hooks/useAuthChecked";
 
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/inbox" element={<Conversation />} />
-                <Route path="/inbox/:id" element={<Inbox />} />
-            </Routes>
-        </Router>
+    const auth = useAuthChecked();
+    return !auth ? (
+      <div>Authentication ......</div>
+    ) : (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/inbox" element={<Conversation />} />
+          <Route path="/inbox/:id" element={<Inbox />} />
+        </Routes>
+      </Router>
     );
 }
 
