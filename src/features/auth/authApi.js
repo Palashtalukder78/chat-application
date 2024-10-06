3557dddb9a5fc6136e1f6db9262595d657c01583
Md Ariful Islam Palash <Md Ariful Islam Palash>
2024-10-06 09:36:37 +0600

    prepared authApi and authSlice

--- src/features/auth/authApi.js
+++ src/features/auth/authApi.js
@@ -1,9 +1,67 @@
 import { apiSlice } from "../api/apiSlice";
+import { userLoggedIn } from "./authSlice";
 
 const authApi = apiSlice.injectEndpoints({
-    endpoints: (builder)=>({
-        //codewill be here
-    })
-})
+  endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (data) => ({
+        url: "/register",
+        method: "POST",
+        body: data,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
 
-export const {} = authApi;
+          localStorage.setItem(
+            "auth",
+            JSON.stringify({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          );
+
+          dispatch(
+            userLoggedIn({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          );
+        } catch (err) {
+          //the error handled in client side
+        }
+      },
+    }),
+    login: builder.mutation({
+      query: (data) => ({
+        url: "/login",
+        method: "POST",
+        body: data,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+
+          localStorage.setItem(
+            "auth",
+            JSON.stringify({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          );
+
+          dispatch(
+            userLoggedIn({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          );
+        } catch (err) {
+          //the error handled in client side
+        }
+      },
+    }),
+  }),
+});
+
+export const { useLoginMutation, useRegisterMutation } = authApi;
